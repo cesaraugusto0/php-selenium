4cb2496f4447a0a863969fa47824bad6d6e760f0
Cesar Augusto <augustocsar22@gmail.com>
2023-11-12 10:25:35 -0300

    adiciona RegistroTest

---
+++ tests/RegistroTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests;
+use PHPUnit\Framework\TestCase;
+
+class RegistroTest extends TestCase
+{
+    public function testQuandoRegistrarNovoUsuarioDeveRedirecionarParaListaDeSeries()
+    {
+        self::assertTrue(true);
+    }
+}
