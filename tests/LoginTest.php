82dfc345b8f91eb3c66bc72efb37234631605094
Cesar Augusto <augustocsar22@gmail.com>
2023-11-12 10:55:52 -0300

    adiciona LoginTest

---
+++ tests/LoginTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests;
+use PHPUnit\Framework\TestCase;
+
+class LoginTest extends TestCase
+{
+    public function testAposLoginUsuarioValidoDeveRedirecionarParaListaDeSeries()
+    {
+        self::assertTrue(true);
+    }
+}
